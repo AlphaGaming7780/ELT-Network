c15659600ee5863c96e17b402df52a3a8a861843
AlphaGaming7780 <69048433+AlphaGaming7780@users.noreply.github.com>
2024-02-28 13:22:31 +0100

    Update UI.cs

--- mod/UI/UI.cs
+++ mod/UI/UI.cs
@@ -1,13 +1,18 @@
 using System;
+using System.Collections.Generic;
 using Colossal.UI.Binding;
+using ExtraLandscapingTools;
+using Game.Prefabs;
 using Game.UI;
-using Game.UI.InGame;
-
+using Unity.Collections;
+using Unity.Entities;
 namespace ELT_Network
 {
 	
 	public class UI : UISystemBase
 	{	
+		private readonly List<string> unTestedPrefab = [""];
+		private static EntityQuery UnTestedPrefabEntityQuery;
         private static GetterValueBinding<bool> showUnTestedObject;
 		protected override void OnCreate() {
 
@@ -16,11 +21,34 @@ namespace ELT_Network
 			AddBinding(showUnTestedObject = new GetterValueBinding<bool>("elt_networks", "showuntestedobject", () => Network.network.ExtensionSettings.ShowUnTestedObject));
 			AddBinding(new TriggerBinding<bool>("elt_networks", "showuntestedobject", new Action<bool>(ShowUnTestedObject)));
 
+			UnTestedPrefabEntityQuery = GetEntityQuery(new EntityQueryDesc
+			{
+				All =
+			   [
+					ComponentType.ReadOnly<ObjectData>(),
+					ComponentType.ReadOnly<UIObjectData>()
+			   ],
+			});
+
 		}
 
 		private void ShowUnTestedObject(bool b) {
 			Network.network.ExtensionSettings.ShowUnTestedObject = b;
 			Network.network.SaveSettings(Network.network.ExtensionSettings);
+
+			NativeArray<Entity> entities =  UnTestedPrefabEntityQuery.ToEntityArray(AllocatorManager.Temp);
+			 
+			foreach(Entity entity in entities) {
+				if(ELT.m_PrefabSystem.TryGetPrefab(entity, out MarkerObjectPrefab markerObjectPrefab) && markerObjectPrefab is not null) {
+					// Plugin.Logger.LogMessage(markerObjectPrefab.name);
+					// we can check the name of the prefab we don't want and use the following code on them.
+					if(Network.network.ExtensionSettings.ShowUnTestedObject) ELT_UI.AddEntityObjectToCategoryUI(entity);
+					else ELT_UI.RemoveEntityObjectFromCategoryUI(entity); 
+				}
+			}
+
+			ExtraLandscapingTools.Patches.ToolbarUISystemPatch.UpdateMenuUI();
+
 			showUnTestedObject.Update();
 		}
 
