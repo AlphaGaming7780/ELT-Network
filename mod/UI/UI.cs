941536a08776a1d267fde02825adc4f81fe9ea6e
kosch104 <konstantin-schwarze@outlook.de>
2024-03-17 01:54:29 +0100

    Improve logic to queries

--- mod/UI/UI.cs
+++ mod/UI/UI.cs
@@ -36,8 +36,6 @@ namespace ELT_Network
 				Any =
 				[
 					ComponentType.ReadOnly<OutsideConnectionData>(),
-					ComponentType.ReadOnly<Game.Objects.ElectricityOutsideConnection>(),
-					ComponentType.ReadOnly<Game.Objects.WaterPipeOutsideConnection>(),
 				]
 			});
 
@@ -58,10 +56,8 @@ namespace ELT_Network
 
 		protected override void OnGameLoaded(Context serializationContext)
 		{
-			Debug.Log("OnGameLoaded with purpose: " + serializationContext.purpose);
 			if (serializationContext.purpose == Purpose.LoadGame)
 			{
-				Debug.Log("Update UI");
 				ShowOutsideConnections(Network.network.ExtensionSettings.ShowOutsideConnections);
 				ShowSpawners(Network.network.ExtensionSettings.ShowSpawners);
 			}
@@ -74,21 +70,7 @@ namespace ELT_Network
 			Network.network.SaveSettings(Network.network.ExtensionSettings);
 
 			NativeArray<Entity> entities =  OutsideConnectionsQuery.ToEntityArray(AllocatorManager.Temp);
-
-			foreach(Entity entity in entities) {
-				if(ELT.m_PrefabSystem.TryGetPrefab(entity, out MarkerObjectPrefab markerObjectPrefab) && markerObjectPrefab is not null) {
-					// Plugin.Logger.LogMessage(markerObjectPrefab.name);
-					// we can check the name of the prefab we don't want and use the following code on them.
-					if(!Network.network.ExtensionSettings.ShowOutsideConnections)
-					{
-						Plugin.Logger.LogMessage(markerObjectPrefab.name + " has been removed from UI");
-						ELT_UI.RemoveEntityObjectFromCategoryUI(entity);
-						continue;
-					}
-					ELT_UI.AddEntityObjectToCategoryUI(entity);
-				}
-			}
-			ExtraLandscapingTools.Patches.ToolbarUISystemPatch.UpdateMenuUI();
+			ToggleEntityInUI(entities, newValue);
 			showOutsideConnections.Update();
 		}
 
@@ -98,21 +80,7 @@ namespace ELT_Network
 			Network.network.SaveSettings(Network.network.ExtensionSettings);
 
 			NativeArray<Entity> entities =  SpawnersEntityQuery.ToEntityArray(AllocatorManager.Temp);
-
-			foreach(Entity entity in entities) {
-				if(ELT.m_PrefabSystem.TryGetPrefab(entity, out MarkerObjectPrefab markerObjectPrefab) && markerObjectPrefab is not null) {
-					// Plugin.Logger.LogMessage(markerObjectPrefab.name);
-					// we can check the name of the prefab we don't want and use the following code on them.
-					if(!Network.network.ExtensionSettings.ShowSpawners)
-					{
-						Plugin.Logger.LogMessage(markerObjectPrefab.name + " has been removed from UI");
-						ELT_UI.RemoveEntityObjectFromCategoryUI(entity);
-						continue;
-					}
-					ELT_UI.AddEntityObjectToCategoryUI(entity);
-				}
-			}
-			ExtraLandscapingTools.Patches.ToolbarUISystemPatch.UpdateMenuUI();
+			ToggleEntityInUI(entities, newValue);
 			showSpawners.Update();
 		}
 
